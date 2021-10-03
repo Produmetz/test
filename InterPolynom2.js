cadd8bd7c8cf3861f6e00bbd5e18871ba8fbacaf
Produmetz <41688728+Produmetz@users.noreply.github.com>
2021-10-03 09:05:49 +0300

    Add files via upload

---
+++ InterPolynom2.js
@@ -0,0 +1,62 @@
+function NtF(x,a){return Number((x).toFixed(a));};
+function ArrayMinusElement(array){let result=[],i=0; while(i<(array.length-1)){result[i]=array[i];};return result;};
+let ArrayDividedDifferences=[];
+
+function Multi(x,y)
+	{
+		let result;
+		result=Number((x*y).toFixed(10));
+		return result;
+	};
+
+function DD(x_1,x_2,y_1,y_2)
+	{
+		let result;
+		result=(y_2-y_1)/(x_2-x_1);
+		result=NtF(result,10);
+		return result;
+	};
+
+function Diff(x,array_x,n)
+	{ 
+		let result=1,i=0;
+		while(i<n+1)
+		{
+			result=Multi(result,(x-array_x[i]));i++;
+		};
+		return result;
+	};	
+function DividedDifferences(array_x,array_y)
+	{ 
+	let result,ArrayDD=[],rang=0,arrayyy=array_y;
+	while(rang<array_x.length)
+		{
+			let i=0;
+			while((i+1)<arrayyy.length)
+				{
+					ArrayDD[i]=DD(array_x[i],array_x[i+1],arrayyy[i],arrayyy[i+1]);i++;
+				};
+			arrayyy=ArrayDD;
+			ArrayDividedDifferences[rang]=ArrayDD[rang];
+			rang++;
+		};	
+	};
+function PolinomNewton(x,array_x,array_y)
+	{
+		let result,i=0;
+		DividedDifferences(array_x,array_y);
+	    
+		result=array_y[0];
+		while(i<ArrayDividedDifferences.length-1)
+		{
+			result=result+Multi(Diff(x,array_x,i),ArrayDividedDifferences[i]);
+			i++;
+		};
+		return result;
+	};
+	
+	let array_x=[0,10,20,30];
+	let array_y=[0,10,20,30];
+	let value;
+	value=PolinomNewton(5,array_x,array_y);
+	alert(value);
